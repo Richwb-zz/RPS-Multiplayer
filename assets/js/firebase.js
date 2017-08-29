1d16f9f3925f716e6a44e2793888eeda72e8efbd
unknown <richwbates@gmail.com>
2017-08-29 14:45:27 -0400

    initial files and configuration of html page and firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyCWcPshquCZB_HSWlK7xkFOH_DNvidpAJg",
+    authDomain: "rpsmp-81e87.firebaseapp.com",
+    databaseURL: "https://rpsmp-81e87.firebaseio.com"
+  };
+  firebase.initializeApp(config);
